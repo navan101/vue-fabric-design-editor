5d1717f069b2495b4c6d433122b9077ed1ea0d5b
navan <van@rowboatsoftware.com>
2024-09-16 17:01:33 +0700

    build demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { createHash } from 'node:crypto'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,5 +13,38 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/vue-fabric-design-editor/' : '/',
+  build: {
+    // cssCodeSplit: false,
+    // sourcemap: false,
+    // target: "esnext",
+    // minify: "esbuild", 
+    minify: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: chunkInfo => {
+          const hash = createHash('md5')
+            .update(chunkInfo.name)
+            .digest('hex').substring(0, 16);
+          return `assets/js/${hash}.js`
+        },
+        entryFileNames: entryInfo => {
+          const hash = createHash('md5')
+            .update(entryInfo.name)
+            .digest('hex').substring(0, 16);
+          return `assets/js/${hash}.js`
+        },
+        assetFileNames: assetInfo => {
+          const hash = createHash('md5')
+            .update(assetInfo.name)
+            .digest('hex').substring(0, 16);
+          return `assets/[ext]/${hash}.[ext]`
+        }
+      },
+      // input: {
+      //   main: fileURLToPath(new URL('index.html', import.meta.url)),
+      // },
+    },
+  },
 })
