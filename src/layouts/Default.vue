65e150a2d0b9d514e2a9ae09fba82deddc6dab61
navan <van@rowboatsoftware.com>
2024-09-16 17:59:58 +0700

    update version build

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,3 +1,15 @@
 <template>
   <Editor />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    console.log('build v1.0.8')
+  }
+})
+</script>
+
+<style>
+</style>
